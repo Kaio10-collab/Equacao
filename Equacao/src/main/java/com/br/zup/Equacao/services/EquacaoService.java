8bcd673552255962f288f698e3fbadecdfd55385
Kaio <kaio.moraes@zup.com.br>
2021-03-22 12:02:47 -0300

    Alterar método de cálculo

--- Equacao/src/main/java/com/br/zup/Equacao/services/EquacaoService.java
+++ Equacao/src/main/java/com/br/zup/Equacao/services/EquacaoService.java
@@ -3,19 +3,20 @@ package com.br.zup.Equacao.services;
 import com.br.zup.Equacao.models.CoeficientesEquacao;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 
 @Service
 public class EquacaoService {
 
     public BigDecimal calcularEquacao(CoeficientesEquacao grau){
+        MathContext mc = new MathContext(10);
 
-        BigDecimal respostaX1 = grau.getA().multiply(new BigDecimal("2"));   //ax²
-        BigDecimal respostaB = grau.getB();  // +bx
-        BigDecimal respostaC = grau.getC();  // +c
-        BigDecimal respostaX = respostaX1.add(respostaB);
-
-        return respostaC.add(respostaX);
-
+        BigDecimal delta =  grau.getB().multiply(grau.getB()).subtract(new BigDecimal(4).multiply(grau.getA()).multiply(grau.getB()));
+        BigDecimal raizQuadradaDelta = delta.sqrt(mc);
+        BigDecimal divisor = new BigDecimal(2).multiply(grau.getA());
+        BigDecimal x1 = (grau.getB().multiply(new BigDecimal(-1).add(raizQuadradaDelta))).divide(divisor);
+        BigDecimal x2 = (grau.getB().multiply(new BigDecimal(-1).subtract(raizQuadradaDelta))).divide(divisor);
+        return calcularEquacao(grau);
     }
 }
